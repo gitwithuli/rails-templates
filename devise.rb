f4771173b9f66b65896c6c37edebbb95b8bec37d
Ulas <61683018+gitwithuli@users.noreply.github.com>
2020-09-26 20:31:02 +0300

    Update devise.rb

--- devise.rb
+++ devise.rb
@@ -209,7 +209,7 @@ after_bundle do
   # Git
   ########################################
   git add: '.'
-  git commit: "-m 'Initial commit with devise template from https://github.com/lewagon/rails-templates'"
+  git commit: "-m 'Initial commit with devise'"
 
   # Fix puma config
   gsub_file('config/puma.rb', 'pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }', '# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }')
